2fd5059e9ba2f5d88a718b4a9093453f84dac327
EfecanAltay <efecan95.gs@gmail.com>
2021-03-29 17:30:31 +0300

    initialize

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+router.get('/mobile',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+console.log(`Running on http://0.0.0.0:3000`);
